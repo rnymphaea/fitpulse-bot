e7bbddcafb0c361de4bcad112ff6b800c9cdce0b
Lvov Alexandr <strangeisdoctor@gmail.com>
2025-05-28 23:33:53 +0300

    removed old folders from root dir

--- storage/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-
-from storage.models import Base
-import config
-
-engine = create_async_engine(config.get('DATABASE_URL'), echo=True)
-
-session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-
